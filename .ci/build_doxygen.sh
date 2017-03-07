a49c5d91334694d1d0853bd1955f9c3894b6cd84
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-03-07 13:23:38 -0500

    Fix constant rebuilding of doxygen

--- .ci/build_doxygen.sh
+++ .ci/build_doxygen.sh
@@ -1,7 +1,7 @@
 DOXYGEN_URL="ftp://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.13.src.tar.gz"
 cd "${DEPS_DIR}"
 
-if [[ ! -f "${DEPS_DIR}/doxygen/bin/doxygen" ]] ; then
+if [[ ! -f "${DEPS_DIR}/doxygen/build/bin/doxygen" ]] ; then
   echo "Downloading Doxygen"
   mkdir -p doxygen
   travis_retry wget --no-check-certificate --quiet -O - "${DOXYGEN_URL}" | tar --strip-components=1 -xz -C doxygen
