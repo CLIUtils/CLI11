e7003673c5fd8c4597024e81621c7f94de73500d
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-03-07 13:04:07 -0500

    Fix single file tests under CodeCov

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -2,12 +2,14 @@
 set -evx
  
 if [ -n "$COVERALLS" ] ; then 
-    export COV1="-DCLI_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug"
+    export COV1="-DCLI_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug -DCLI_SINGLE_FILE_TESTS=OFF"
+else
+    export COV1="-DCLI_SINGLE_FILE_TESTS=ON"
 fi
 
 
 mkdir build || true
 cd build
-cmake .. -DCLI_SINGLE_FILE_TESTS=ON $COV1
+cmake .. $COV1
 cmake --build .
 ctest --output-on-failure
