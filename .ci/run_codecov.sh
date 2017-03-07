5f0825a4cc6ea954c058d4bcdbf9314b47ca561d
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-03-07 13:09:41 -0500

    Seperating coverage steps

--- .ci/run_codecov.sh
+++ .ci/run_codecov.sh
@@ -1,4 +1,10 @@
 cd ${TRAVIS_BUILD_DIR}
+
+cd build
+cmake .. -DCLI_SINGLE_FILE_TESTS=OFF -DCLI_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug
+cmake --build . --target CLI_coverage
+ctest --output-on-failure
+
 lcov --directory . --capture --output-file coverage.info # capture coverage info
 lcov --remove coverage.info '/usr/*' --output-file coverage.info # filter out system
 lcov --list coverage.info #debug info
--- .ci/travis.sh
+++ .ci/travis.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env sh
 set -evx
- 
-if [ -n "$COVERALLS" ] ; then 
-    export COV1="-DCLI_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug -DCLI_SINGLE_FILE_TESTS=OFF"
-else
-    export COV1="-DCLI_SINGLE_FILE_TESTS=ON"
-fi
-
 
 mkdir build || true
 cd build
-cmake .. $COV1
+cmake .. -DCLI_SINGLE_FILE_TESTS=ON
 cmake --build .
 ctest --output-on-failure
