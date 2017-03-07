9b0f93897a3a5a2ba8cb81d4f4e806f984e46aa7
Henry Schreiner <hschrein@cern.ch>
2017-03-07 14:13:54 -0500

    Adding coverage, tested locally

--- .ci/run_codecov.sh
+++ .ci/run_codecov.sh
@@ -2,11 +2,11 @@ cd ${TRAVIS_BUILD_DIR}
 
 cd build
 cmake .. -DCLI_SINGLE_FILE_TESTS=OFF -DCLI_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug
+cmake --build .
 cmake --build . --target CLI_coverage
-ctest --output-on-failure
 
 lcov --directory . --capture --output-file coverage.info # capture coverage info
-lcov --remove coverage.info '/usr/*' --output-file coverage.info # filter out system
+lcov --remove coverage.info '*/tests/*' '*gtest*' '*gmock*' '/usr/*' --output-file coverage.info # filter out system
 lcov --list coverage.info #debug info
 # Uploading report to CodeCov
 bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
