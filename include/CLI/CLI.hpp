9301925359559b496141d042dfac51ad21ee6774
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-05-31 12:21:42 -0400

    Fix single file include

--- include/CLI/CLI.hpp
+++ include/CLI/CLI.hpp
@@ -4,11 +4,13 @@
 // file LICENSE or https://github.com/CLIUtils/CLI11 for details.
 
 // CLI Library includes
-#include "CLI/App.hpp"
+// Order is important for combiner script
+
 #include "CLI/Error.hpp"
-#include "CLI/Ini.hpp"
-#include "CLI/Option.hpp"
-#include "CLI/Split.hpp"
-#include "CLI/StringTools.hpp"
 #include "CLI/TypeTools.hpp"
+#include "CLI/StringTools.hpp"
+#include "CLI/Split.hpp"
+#include "CLI/Ini.hpp"
 #include "CLI/Validators.hpp"
+#include "CLI/Option.hpp"
+#include "CLI/App.hpp"
