3da480792b18f0debd181270372cb2d560494a52
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-06-09 08:31:47 -0400

    Adding enum example

---
+++ examples/enum.cpp
@@ -0,0 +1,23 @@
+#include <CLI/CLI.hpp>
+
+enum Level : std::int32_t {
+    High,
+    Medium,
+    Low
+};
+
+int main(int argc, char** argv) {
+    CLI::App app;
+
+    Level level;
+    app.add_set("-l,--level", level, {High, Medium, Low}, "Level settings")
+        ->set_type_name("enum/Level in {High=0, Medium=1, Low=2}");
+
+    try {
+        app.parse(argc, argv);
+    } catch (CLI::Error const& e) {
+        app.exit(e);
+    }
+    return 0;
+}
+
