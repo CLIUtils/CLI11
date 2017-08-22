8bf8a695de2779f0f2a651892c1d934f384d377d
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-08-22 12:07:15 -0700

    Avoid examples in coverage

--- .ci/run_codecov.sh
+++ .ci/run_codecov.sh
@@ -7,7 +7,7 @@ cmake --build . -- -j2
 cmake --build . --target CLI_coverage
 
 lcov --directory . --capture --output-file coverage.info # capture coverage info
-lcov --remove coverage.info '*/tests/*' '*gtest*' '*gmock*' '/usr/*' --output-file coverage.info # filter out system
+lcov --remove coverage.info '*/tests/*' '*/examples/*' '*gtest*' '*gmock*' '/usr/*' --output-file coverage.info # filter out system
 lcov --list coverage.info #debug info
 # Uploading report to CodeCov
 bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
