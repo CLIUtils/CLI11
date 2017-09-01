a56fd3c3a86c119c70352bef492bd3f0eec0e90f
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-09-01 11:51:08 -0400

    Support clang-format 3.9

--- .ci/check_style.sh
+++ .ci/check_style.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 set -evx
 
+clang-format --version
+
 git ls-files -- '*.cpp' '*.hpp' | xargs clang-format -i -style=file
-git diff --exit-code
+
+git diff --exit-code --color
 
 mkdir build || true
 cd build
