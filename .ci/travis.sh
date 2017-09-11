8233e5ce22862b22e785c117cd249261239e700c
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-09-11 10:32:38 -0400

    Better GCC 4.7 support, travis prepared for C++17 (not used yet)

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -6,9 +6,13 @@ cd build
 cmake .. -DCLI_CXX_STD=11 -DCLI_SINGLE_FILE_TESTS=ON -DCMAKE_BUILD_TYPE=Debug
 cmake --build . -- -j2
 ctest --output-on-failure
-
-if [ "$CLI_CXX_STD" == "14" ] ; then
+if [ -n "$CLI_CXX_STD" ] && [ "$CLI_CXX_STD" -ge "14" ] ; then
     cmake .. -DCLI_CXX_STD=14 -DCLI_SINGLE_FILE_TESTS=ON -DCMAKE_BUILD_TYPE=Debug
+    cmake --build . -- -j2
+    ctest --output-on-failure
+fi
+if [ -n "$CLI_CXX_STD" ] && [ "$CLI_CXX_STD" -ge "17" ] ; then
+    cmake .. -DCLI_CXX_STD=17 -DCLI_SINGLE_FILE_TESTS=ON -DCMAKE_BUILD_TYPE=Debug
     cmake --build . -- -j2
     ctest --output-on-failure
 fi
