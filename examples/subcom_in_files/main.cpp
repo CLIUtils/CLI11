6d26440b6ed9f65c15140269746901afcb4befba
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-10-27 13:13:44 -0400

    Clean up and shared ptr

--- examples/subcom_in_files/main.cpp
+++ examples/subcom_in_files/main.cpp
@@ -8,9 +8,12 @@ int main( int argc, char** argv ) {
     CLI::App app{"..."};
 
     // Call the setup functions for the subcommands.
-    // They return their option structs, so that the variables to which the options bind
-    // are still alive when `app.parse()` is called.
-    auto subcommand_a_opt = setup_subcommand_a( app );
+    // They are kept alive by a shared pointer in the 
+    // lambda function held by CLI11
+    setup_subcommand_a( app );
+
+    // Make sure we get at least one subcommand
+    app.require_subcommand();
 
     // More setup if needed, i.e., other subcommands etc.
 
--- examples/subcom_in_files/subcommand_a.cpp
+++ examples/subcom_in_files/subcommand_a.cpp
@@ -4,26 +4,27 @@
 
 #include "subcommand_a.hpp"
 
-/// Set up a subcommand and return a unique ptr to a struct that holds all its options.
+/// Set up a subcommand and capture a shared_ptr to a struct that holds all its options.
 /// The variables of the struct are bound to the CLI options.
-/// We use a unique ptr so that the addresses of the variables are stable for binding,
-/// and return it to the caller (main), so that the object itself stays alive.
-std::unique_ptr<SubcommandAOptions> setup_subcommand_a( CLI::App& app )
-{
+/// We use a shared ptr so that the addresses of the variables remain for binding,
+/// You could return the shared pointer if you wanted to access the values in main.
+void setup_subcommand_a( CLI::App& app ) {
     // Create the option and subcommand objects.
-    auto opt = std::unique_ptr<SubcommandAOptions>(new SubcommandAOptions());
+    auto opt = std::make_shared<SubcommandAOptions>();
     auto sub = app.add_subcommand( "subcommand_a", "performs subcommand a", true );
 
     // Add options to sub, binding them to opt.
-    sub->add_option("-f,--file", opt->file, "File name");
+    sub->add_option("-f,--file", opt->file, "File name")->required();
     sub->add_flag("--with-foo", opt->with_foo, "Counter");
 
     // Set the run function as callback to be called when this subcommand is issued.
-    sub->set_callback( [&]() {
+    sub->set_callback([opt]() {
         run_subcommand_a( *opt );
     });
-
-    return opt;
+    
+    // Note: In C++14, you could make a unique pointer, then pass it into the lambda function via
+    // a move. That's slightly more elegant, but you won't be able to see the runtime difference
+    // in skipping one mutex check for shared_ptr.
 }
 
 /// The function that runs our code.
