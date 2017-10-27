8cdf1c865139ed08b25c6939779920b422b466b1
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-10-27 14:07:10 -0400

    Style updates

--- examples/subcom_in_files/main.cpp
+++ examples/subcom_in_files/main.cpp
@@ -4,13 +4,13 @@
 
 #include "subcommand_a.hpp"
 
-int main( int argc, char** argv ) {
+int main(int argc, char **argv) {
     CLI::App app{"..."};
 
     // Call the setup functions for the subcommands.
-    // They are kept alive by a shared pointer in the 
+    // They are kept alive by a shared pointer in the
     // lambda function held by CLI11
-    setup_subcommand_a( app );
+    setup_subcommand_a(app);
 
     // Make sure we get at least one subcommand
     app.require_subcommand();
