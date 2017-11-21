c1109e5c594c8b35b51aac1690e7fdc9c3479fe3
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-11-21 15:48:56 -0500

    Moving to newer CMake (3.9) on Travis

--- .ci/build_cmake.sh
+++ .ci/build_cmake.sh
@@ -1,9 +1,14 @@
-if [ "$TRAVIS_OS_NAME" = "linux" ] ; then CMAKE_URL="https://cmake.org/files/v3.7/cmake-3.7.2-Linux-x86_64.tar.gz" ; fi
-if [ "$TRAVIS_OS_NAME" = "osx" ] ; then CMAKE_URL="https://cmake.org/files/v3.7/cmake-3.7.2-Darwin-x86_64.tar.gz" ; fi
+CMAKE_VERSION=3.9.6
+CMAKE_MVERSION=${CMAKE_VERSION%.*}
+# Non Bash version:
+# echo CMAKE_MVERSION=`$var | awk -F"." '{ print $1"."$2 }'`
+
+if [ "$TRAVIS_OS_NAME" = "linux" ] ; then CMAKE_URL="https://cmake.org/files/v${CMAKE_MVERSION}/cmake-${CMAKE_VERSION}-Linux-x86_64.tar.gz" ; fi
+if [ "$TRAVIS_OS_NAME" = "osx" ] ; then CMAKE_URL="https://cmake.org/files/v$CMAKE_MVERSION/cmake-$CMAKE_VERSION-Darwin-x86_64.tar.gz" ; fi
 cd "${DEPS_DIR}"
 
 if [[ ! -f "${DEPS_DIR}/cmake/bin/cmake" ]] ; then
-  echo "Downloading CMake"
+  echo "Downloading CMake $CMAKE_VERSION"
   mkdir -p cmake
   travis_retry wget --no-check-certificate --quiet -O - "${CMAKE_URL}" | tar --strip-components=1 -xz -C cmake
 fi
