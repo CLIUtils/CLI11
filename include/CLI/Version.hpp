437dbab140cb1618cc9ba1d13087cedfd2621c15
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-12-01 07:34:52 -0500

    Fix style

--- include/CLI/Version.hpp
+++ include/CLI/Version.hpp
@@ -3,7 +3,6 @@
 // Distributed under the 3-Clause BSD License.  See accompanying
 // file LICENSE or https://github.com/CLIUtils/CLI11 for details.
 
-
 namespace CLI {
 
 // Note that all code in CLI11 must be in a namespace, even if it just a define.
@@ -12,6 +11,5 @@ namespace CLI {
 #define CLI11_VERSION_MINOR 3
 #define CLI11_VERSION_PATCH 0
 #define CLI11_VERSION "1.3.0"
-
 
 } // namespace CLI
