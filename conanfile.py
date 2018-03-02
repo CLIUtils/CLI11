c4ab78acc39769661fc6962b830490fb8bfe5ca7
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2018-03-02 10:46:28 +0100

    Adding conan build

---
+++ conanfile.py
@@ -0,0 +1,25 @@
+from conans import ConanFile, CMake
+
+class HelloConan(ConanFile):
+    name = "CLI11"
+    version = "1.3.0"
+    url = "https://github.com/CLIUtils/CLI11"
+    settings = "os", "compiler", "arch", "build_type"
+    license = "BSD 3 clause"
+    description = "Command Line Interface toolkit for C++11"
+
+    exports_sources = "LICENCE",  "include/*", "cmake/*", "CMakeLists.txt", "tests/*"
+
+    def build(self): # this is not building a library, just tests
+        cmake = CMake(self)
+        cmake.definitions["CLI_EXAMPLES"] = "OFF"
+        cmake.definitions["CLI_SINGLE_FILE"] = "OFF"
+        cmake.configure()
+        cmake.build()
+        cmake.test()
+
+    def package(self):
+        self.copy("*.hpp")
+
+    def package_id(self):
+        self.info.header_only()
