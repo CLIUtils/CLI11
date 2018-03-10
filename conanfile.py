9be6536563f7bd2a4d32a59d059cbdaba1afa6b6
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2018-03-10 09:32:25 +0100

    Conan file now gets version from Version.hpp file too

--- conanfile.py
+++ conanfile.py
@@ -1,14 +1,24 @@
 from conans import ConanFile, CMake
+from conans.tools import load
+import re
+
+def get_version():
+    try:
+        content = load("include/CLI/Version.hpp")
+        version = re.search(r'#define CLI11_VERSION "(.*)"', content).group(1)
+        return version
+    except Exception:
+        return None
 
 class HelloConan(ConanFile):
     name = "CLI11"
-    version = "1.4.0"
+    version = get_version()
     url = "https://github.com/CLIUtils/CLI11"
     settings = "os", "compiler", "arch", "build_type"
     license = "BSD 3 clause"
     description = "Command Line Interface toolkit for C++11"
 
-    exports_sources = "LICENCE",  "include/*", "cmake/*", "CMakeLists.txt", "tests/*"
+    exports_sources = "LICENSE", "README.md", "include/*", "cmake/*", "CMakeLists.txt", "tests/*"
 
     def build(self): # this is not building a library, just tests
         cmake = CMake(self)
