e0bcc36e16d46ff27928a04f2e404b994f065ea3
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2018-04-04 08:48:14 +0200

    Adding better optional, check for Boost

--- include/CLI/Optional.hpp
+++ include/CLI/Optional.hpp
@@ -11,31 +11,64 @@
 #ifdef __has_include
 #if defined(CLI11_CPP17) && __has_include(<optional>)
 #include <optional>
-#define CLI11_OPTIONAL
-namespace CLI {
-using std::experimental::optional;
-} // namespace CLI
-#elif defined(CPP11_CPP14) && __has_include(<experimental/optional>)
+#ifdef __cpp_lib_optional
+#define CLI11_STD_OPTIONAL
+#endif
+#endif
+#if defined(CPP11_CPP14) && __has_include(<experimental/optional>)
 #include <experimental/optional>
-#define CLI11_OPTIONAL
-namespace CLI {
-using std::optional;
-} // namespace CLI
+#ifdef __cpp_lib_experimental_optional
+#define CLI11_EXPERIMENTAL_OPTIONAL
+#endif
+#endif
+#if __has_include(<boost/optional>)
+#include <boost/optional.hpp>
+#define CLI11_BOOST_OPTIONAL
 #endif
 #endif
 // [CLI11:verbatim]
 
 namespace CLI {
 
-#ifdef CLI11_OPTIONAL
+#ifdef CLI11_STD_OPTIONAL
+template <typename T> std::istream &operator>>(std::istream &in, std::optional<T> &val) {
+    T v;
+    in >> v;
+    val = v;
+    return in;
+}
+#endif
 
-template <typename T> std::istream &operator>>(std::istream &in, optional<T> &val) {
+#ifdef CLI11_EXPERIMENTAL_OPTIONAL
+template <typename T> std::istream &operator>>(std::istream &in, std::experimental::optional<T> &val) {
     T v;
     in >> v;
     val = v;
     return in;
 }
 
+#ifdef CLI11_BOOST_OPTIONAL
+template <typename T> std::istream &operator>>(std::istream &in, boost::optional<T> &val) {
+    T v;
+    in >> v;
+    val = v;
+    return in;
+}
+#endif
+#endif
+
+// Export the best optional to the CLI namespace
+#if defined(CLI11_STD_OPTIONAL)
+using std::optional;
+#elif CLI11_EXPERIMENTAL_OPTIONAL
+using std::experimental::optional;
+#elif CLI11_BOOST_OPTIONAL
+using boost::optionall
+#endif
+
+// This is true if any optional is found
+#if defined(CLI11_STD_OPTIONAL) || defined(CLI11_EXPERIMENTAL_OPTIONAL) || defined(CLI11_BOOST_OPTIONAL)
+#define CLI11_OPTIONAL
 #endif
 
 } // namespace CLI
