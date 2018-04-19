7a6cd04ec4b327ffc3ccc75a7a7fcac3f37800bb
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2018-04-19 12:52:20 +0200

    Bump version to 1.5.3

--- include/CLI/Version.hpp
+++ include/CLI/Version.hpp
@@ -9,7 +9,7 @@ namespace CLI {
 
 #define CLI11_VERSION_MAJOR 1
 #define CLI11_VERSION_MINOR 5
-#define CLI11_VERSION_PATCH 2
-#define CLI11_VERSION "1.5.2"
+#define CLI11_VERSION_PATCH 3
+#define CLI11_VERSION "1.5.3"
 
 } // namespace CLI
