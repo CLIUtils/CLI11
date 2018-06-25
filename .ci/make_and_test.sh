2ae5598d9570a63c8d9483ef776c3bb358896459
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2018-06-25 12:03:47 +0200

    Adding example with json config

--- .ci/make_and_test.sh
+++ .ci/make_and_test.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 echo -en "travis_fold:start:script.build\\r"
 echo "Building..."
+STD=$1
+shift
 set -evx
+
 
 mkdir -p build
 cd build
-cmake .. -DCLI11_CXX_STD=$1 -DCLI11_SINGLE_FILE_TESTS=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
+cmake .. -DCLI11_CXX_STD=$STD -DCLI11_SINGLE_FILE_TESTS=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER_LAUNCHER=ccache $@
 cmake --build . -- -j2
 
 set +evx
