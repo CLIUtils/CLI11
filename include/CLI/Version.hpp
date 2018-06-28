e32656e46d0dba79a747a25637a8b38a14a12a8b
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2018-06-28 14:36:02 +0200

    Version bump

--- include/CLI/Version.hpp
+++ include/CLI/Version.hpp
@@ -6,8 +6,8 @@
 // [CLI11:verbatim]
 
 #define CLI11_VERSION_MAJOR 1
-#define CLI11_VERSION_MINOR 5
-#define CLI11_VERSION_PATCH 4
-#define CLI11_VERSION "1.5.4"
+#define CLI11_VERSION_MINOR 6
+#define CLI11_VERSION_PATCH 0
+#define CLI11_VERSION "1.6.0"
 
 // [CLI11:verbatim]
