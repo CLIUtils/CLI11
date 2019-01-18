9bd07fc4b9e8bf3c436d412a413f232a7fe7454d
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2019-01-18 15:59:06 +0100

    Getting a bit closer to Version 1.7

--- include/CLI/Version.hpp
+++ include/CLI/Version.hpp
@@ -6,8 +6,8 @@
 // [CLI11:verbatim]
 
 #define CLI11_VERSION_MAJOR 1
-#define CLI11_VERSION_MINOR 6
-#define CLI11_VERSION_PATCH 2
-#define CLI11_VERSION "1.6.2"
+#define CLI11_VERSION_MINOR 7
+#define CLI11_VERSION_PATCH 0
+#define CLI11_VERSION "1.7.0"
 
 // [CLI11:verbatim]
