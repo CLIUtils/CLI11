b3ad215b6fbb0e8c66d254765b53711687fd7c86
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2019-01-22 14:14:01 +0100

    Fixing format

--- include/CLI/Option.hpp
+++ include/CLI/Option.hpp
@@ -518,7 +518,10 @@ class Option : public OptionBase<Option> {
     const std::string &get_description() const { return description_; }
 
     /// Set the description
-    Option* description(const std::string &description) { description_ = description; return this; }
+    Option *description(const std::string &description) {
+        description_ = description;
+        return this;
+    }
 
     ///@}
     /// @name Help tools
