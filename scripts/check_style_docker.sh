048f968504e3157ab2348f111221271a9dd86992
Rafi Wiener <rafiwiener@gmail.com>
2019-02-06 16:15:31 +0200

    Add support for delimiter when parsing vector (#209)
    This commit allows parsing any char separated list given by the user.
    E.g app -a 1,2,3
    std::vector<int> params;
    app.add_option("-a", params, "Parse the params", ',');
    
    Signed-off-by: Rafi Wiener <rafiw@mellanox.com>
    
    add tests for delimiter parsing
    
    Signed-off-by: Rafi Wiener <rafiw@mellanox.com>
    
    Fixing style, adding docker version of clang-format

---
+++ scripts/check_style_docker.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# Also good but untagged: CLANG_FORMAT=unibeautify/clang-format
+CLANG_FORMAT=saschpe/clang-format:5.0.1
+
+set -evx
+
+docker run -it ${CLANG_FORMAT} --version
+docker run -it -v "$(pwd)":/workdir -w /workdir ${CLANG_FORMAT} -style=file -sort-includes -i $(git ls-files -- '*.cpp' '*.hpp')
+
+git diff --exit-code --color
+
+set +evx
