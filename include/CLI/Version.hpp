13becaddb657eacd090537719a669d66d393b8b2
Henry Schreiner <henry.fredrick.schreiner@cern.ch>
2019-05-20 14:23:11 +0200

    Version 1.8 (#280)
    * Version bump
    
    * Update README
    
    * Apply suggestions from code review
    
    Co-Authored-By: Philip Top <top1@llnl.gov>
    
    * Adding mark for deprecated items

--- include/CLI/Version.hpp
+++ include/CLI/Version.hpp
@@ -6,8 +6,8 @@
 // [CLI11:verbatim]
 
 #define CLI11_VERSION_MAJOR 1
-#define CLI11_VERSION_MINOR 7
-#define CLI11_VERSION_PATCH 1
-#define CLI11_VERSION "1.7.1"
+#define CLI11_VERSION_MINOR 8
+#define CLI11_VERSION_PATCH 0
+#define CLI11_VERSION "1.8.0"
 
 // [CLI11:verbatim]
