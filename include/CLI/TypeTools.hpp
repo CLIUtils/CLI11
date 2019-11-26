1e65ac26b3a0709f4258f03309e7cacb605b10d7
Philip Top <phlptp@gmail.com>
2019-11-25 19:25:55 -0800

    try a simpler version of the value_string using const reference for all overloads

--- include/CLI/TypeTools.hpp
+++ include/CLI/TypeTools.hpp
@@ -260,26 +260,20 @@ std::string checked_to_string(T &&) {
     return std::string{};
 }
 /// get a string as a convertible value for arithmetic types
-template <
-    typename T,
-    enable_if_t<std::is_arithmetic<typename std::remove_reference<T>::type>::value, detail::enabler> = detail::dummy>
-std::string value_string(T &&value) {
-    return std::to_string(std::forward<T>(value));
+template <typename T, enable_if_t<std::is_arithmetic<T>::value, detail::enabler> = detail::dummy>
+std::string value_string(const T &value) {
+    return std::to_string(value);
 }
 /// get a string as a convertible value for enumerations
-template <typename T,
-          enable_if_t<std::is_enum<typename std::remove_reference<T>::type>::value, detail::enabler> = detail::dummy>
-std::string value_string(T &&value) {
-    return std::to_string(
-        static_cast<typename std::underlying_type<typename std::remove_reference<T>::type>::type>(value));
+template <typename T, enable_if_t<std::is_enum<T>::value, detail::enabler> = detail::dummy>
+std::string value_string(const T &value) {
+    return std::to_string(static_cast<typename std::underlying_type<T>::type>(value));
 }
 /// for other types just use the regular to_string function
 template <typename T,
-          enable_if_t<!std::is_enum<typename std::remove_reference<T>::type>::value &&
-                          !std::is_arithmetic<typename std::remove_reference<T>::type>::value,
-                      detail::enabler> = detail::dummy>
-std::string value_string(T &&value) {
-    return to_string(std::forward<T>(value));
+          enable_if_t<!std::is_enum<T>::value && !std::is_arithmetic<T>::value, detail::enabler> = detail::dummy>
+std::string value_string(const T &value) {
+    return to_string(value);
 }
 
 /// This will only trigger for actual void type
