884540ea7be37ea641fe8fbfc17054a5d5f06eba
Philip Top <phlptp@gmail.com>
2019-12-02 16:50:12 -0800

    add example comments

--- examples/retired.cpp
+++ examples/retired.cpp
@@ -1,6 +1,7 @@
 #include "CLI/CLI.hpp"
 #include <vector>
 
+// This example show the usage of the retired and deprecated option helper methods
 int main(int argc, char **argv) {
 
     CLI::App app("example for retired/deprecated options");
@@ -12,9 +13,14 @@ int main(int argc, char **argv) {
 
     app.add_option("--not_deprecated", x);
 
+    // specify that a no existing option is retired
     CLI::retire_option(app, "--retired_option");
+
+    // specify that an existing option is retired and non-functional this will replace the option with another that
+    // behaves the same but does nothing
     CLI::retire_option(app, opt1);
 
+    // deprecate an existing option and specify a recommended replacement
     CLI::deprecate_option(opt2, "--not_deprecated");
 
     CLI11_PARSE(app, argc, argv);
