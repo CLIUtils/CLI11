60e2932356f049ba0313559c3d512bb1bc8e2b72
Isabella Muerte <63051+slurps-mad-rips@users.noreply.github.com>
2020-01-08 12:15:11 -0500

    CMake updates from @slurps-mad-rips

--- cmake/AddGoogletest.cmake
+++ cmake/AddGoogletest.cmake
@@ -37,6 +37,10 @@ macro(add_gtest TESTNAME)
     else()
         add_test(${TESTNAME} ${TESTNAME})
         set_target_properties(${TESTNAME} PROPERTIES FOLDER "Tests")
+        if (CLI11_FORCE_LIBCXX)
+           set_property(TARGET ${T} APPEND_STRING
+             PROPERTY LINK_FLAGS -stdlib=libc++)
+         endif()
     endif()
 
 endmacro()
