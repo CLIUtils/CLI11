3e901409886367a01c9c71cb483f4629469341fa
Henry Schreiner <henryschreineriii@gmail.com>
2020-01-08 13:02:48 -0500

    Adding action check

---
+++ .github/actions/cmake_config/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+
+set -ex
+
+mkdir -p cmake_dir
+mkdir -p build_tmp
+mkdir -p cmake_sources
+rm -rf cmake_dir/* build_tmp/*
+
+v=$1
+fn=cmake-$v-Linux-x86_64.tar.gz
+
+if [ ! -f cmake_souces/$fn ]; then
+    wget -qO cmake_sources/$fn "https://cmake.org/files/v${v%.*}/$fn"
+fi
+
+tar -xzf cmake_sources/$fn --strip-components=1 -C $PWD/cmake_dir
+
+export PATH=$PWD/cmake_dir/bin:$PATH
+
+cmake --version
+
+cd build_tmp && cmake .. $2
