192b54bf1cb7b09891586f33c765c8a0e23ccf36
Christoph Bachhuber <cbachhuber89@gmail.com>
2020-02-10 20:05:09 +0100

    Implement suggestions from code review

--- examples/inter_argument_order.cpp
+++ examples/inter_argument_order.cpp
@@ -1,3 +1,9 @@
+// Copyright (c) 2017-2020, University of Cincinnati, developed by Henry Schreiner
+// under NSF AWARD 1414736 and by the respective contributors.
+// All rights reserved.
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
 #include <CLI/CLI.hpp>
 #include <algorithm>
 #include <iostream>
--- include/CLI/Timer.hpp
+++ include/CLI/Timer.hpp
@@ -1,3 +1,9 @@
+// Copyright (c) 2017-2020, University of Cincinnati, developed by Henry Schreiner
+// under NSF AWARD 1414736 and by the respective contributors.
+// All rights reserved.
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
 #pragma once
 
 // On GCC < 4.8, the following define is often missing. Due to the
