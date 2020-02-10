34bfa386917c2bed3ffe979580b07064a5586936
Christoph Bachhuber <cbachhuber89@gmail.com>
2020-02-10 20:28:47 +0100

    Add copyright and copyright check (#424)
    * Add copyright check
    
    * Remove old license message
    
    * Implement suggestions from code review

--- include/CLI/Macros.hpp
+++ include/CLI/Macros.hpp
@@ -1,7 +1,10 @@
-#pragma once
+// Copyright (c) 2017-2020, University of Cincinnati, developed by Henry Schreiner
+// under NSF AWARD 1414736 and by the respective contributors.
+// All rights reserved.
+//
+// SPDX-License-Identifier: BSD-3-Clause
 
-// Distributed under the 3-Clause BSD License.  See accompanying
-// file LICENSE or https://github.com/CLIUtils/CLI11 for details.
+#pragma once
 
 // [CLI11:verbatim]
 
