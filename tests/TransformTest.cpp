638eb0ddf7d3b2881452b5d3d64db830cb28246f
Josh Soref <jsoref@users.noreply.github.com>
2020-02-11 01:41:32 -0500

    spelling: default

--- tests/TransformTest.cpp
+++ tests/TransformTest.cpp
@@ -104,7 +104,7 @@ TEST_F(TApp, EnumCheckedTransform) {
 }
 
 // from jzakrzewski Issue #330
-TEST_F(TApp, EnumCheckedDefualtTransform) {
+TEST_F(TApp, EnumCheckedDefaultTransform) {
     enum class existing : std::int16_t { abort, overwrite, remove };
     app.add_option("--existing", "What to do if file already exists in the destination")
         ->transform(
