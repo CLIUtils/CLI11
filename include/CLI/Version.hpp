f1349f12e3177608fa1c2f339cae5cfb6e99dbe5
Henry Schreiner <henryschreineriii@gmail.com>
2020-06-20 10:29:27 -0400

    Bump to Version 1.9.1

--- include/CLI/Version.hpp
+++ include/CLI/Version.hpp
@@ -10,7 +10,7 @@
 
 #define CLI11_VERSION_MAJOR 1
 #define CLI11_VERSION_MINOR 9
-#define CLI11_VERSION_PATCH 0
-#define CLI11_VERSION "1.9.0"
+#define CLI11_VERSION_PATCH 1
+#define CLI11_VERSION "1.9.1"
 
 // [CLI11:verbatim]
