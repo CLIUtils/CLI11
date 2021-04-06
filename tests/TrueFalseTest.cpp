14ec6370a34f4c93cc47cadbd3a93fe390ab4baf
Henry Schreiner <henryschreineriii@gmail.com>
2021-04-06 15:59:06 -0400

    DO NOT MERGE: demo of broken GENERATE on MSVC 2015

--- tests/TrueFalseTest.cpp
+++ tests/TrueFalseTest.cpp
@@ -8,7 +8,7 @@
 
 TEST_CASE_METHOD(TApp, "True Bool Option", "[bool][flag]") {
     // Strings needed here due to MSVC 2015.
-    auto param = GENERATE(as<std::string>{}, "true", "on", "True", "ON");
+    auto param = GENERATE("true", "on", "True", "ON");
     bool value{false};  // Not used, but set just in case
     app.add_option("-b,--bool", value);
     args = {"--bool", param};
@@ -18,7 +18,7 @@ TEST_CASE_METHOD(TApp, "True Bool Option", "[bool][flag]") {
 }
 
 TEST_CASE_METHOD(TApp, "False Bool Option", "[bool][flag]") {
-    auto param = GENERATE(as<std::string>{}, "false", "off", "False", "OFF");
+    auto param = GENERATE("false", "off", "False", "OFF");
 
     bool value{true};  // Not used, but set just in case
     app.add_option("-b,--bool", value);
