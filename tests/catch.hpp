de28d11f7c9959aa59617b9ce2315b35434658ae
Henry Schreiner <henry.fredrick.schreiner@cern.ch>
2021-10-13 11:45:44 -0400

    tests: support external Catch2 (#653)

---
+++ tests/catch.hpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2017-2021, University of Cincinnati, developed by Henry Schreiner
+// under NSF AWARD 1414736 and by the respective contributors.
+// All rights reserved.
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+#pragma once
+
+#include <catch2/catch.hpp>
