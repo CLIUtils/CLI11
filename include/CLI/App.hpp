6169ab2bb311d2406179e0ebfbc84e6fcba028b6
Philip Top <phlptp@gmail.com>
2022-05-22 06:50:57 -0700

    fix the issue where subcommand callbacks would be triggered multiple times if specified as configurable.

--- include/CLI/App.hpp
+++ include/CLI/App.hpp
@@ -2413,7 +2413,7 @@ class App {
         }
         // check for section close
         if(item.name == "--") {
-            if(configurable_) {
+            if(configurable_ && parse_complete_callback_) {
                 _process_callbacks();
                 _process_requirements();
                 run_callback();
