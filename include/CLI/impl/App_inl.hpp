00b4870dd9c0127f8dc84bd9717db9d1d6c6fe16
Philip Top <phlptp@gmail.com>
2022-09-29 06:24:31 -0700

    fix warning

--- include/CLI/impl/App_inl.hpp
+++ include/CLI/impl/App_inl.hpp
@@ -1392,7 +1392,7 @@ CLI11_INLINE bool App::_parse_single_config(const ConfigItem &item, std::size_t 
                 op->add_result(res);
                 return true;
             }
-            if(item.inputs.size() > op->get_items_expected_max()) {
+            if(static_cast<int>(item.inputs.size()) > op->get_items_expected_max()) {
                 if(op->get_items_expected_max() > 1) {
                     throw ArgumentMismatch::AtMost(item.fullname(), op->get_items_expected_max(), item.inputs.size());
                 }
