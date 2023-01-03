39a5f1981e9395e82be863e77b4b0c69239ec338
Philip Top <phlptp@gmail.com>
2023-01-03 14:29:37 -0800

    chore: update copyright year to 2023 (#825)

--- tests/mesonTest/main.cpp
+++ tests/mesonTest/main.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2021, University of Cincinnati, developed by Henry Schreiner
+// Copyright (c) 2017-2023, University of Cincinnati, developed by Henry Schreiner
 // under NSF AWARD 1414736 and by the respective contributors.
 // All rights reserved.
 //
