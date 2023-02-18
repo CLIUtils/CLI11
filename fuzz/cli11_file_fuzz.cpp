d36c9e97380ed52a94f870469251d84b0d8d0baf
Philip Top <phlptp@gmail.com>
2023-02-18 07:13:10 -0800

    update license to match rest of code base

--- fuzz/cli11_file_fuzz.cpp
+++ fuzz/cli11_file_fuzz.cpp
@@ -1,9 +1,8 @@
-/*
-Copyright (c) 2019-2023,
-Lawrence Livermore National Security, LLC;
-See the top-level NOTICE for additional details. All rights reserved.
-SPDX-License-Identifier: BSD-3-Clause
-*/
+// Copyright (c) 2017-2023, University of Cincinnati, developed by Henry Schreiner
+// under NSF AWARD 1414736 and by the respective contributors.
+// All rights reserved.
+//
+// SPDX-License-Identifier: BSD-3-Clause
 
 #include "fuzzApp.hpp"
 #include <CLI/CLI.hpp>
