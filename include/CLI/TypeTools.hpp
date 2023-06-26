0f4ace136aae8fd3e0804fa9434c0433229f5d72
Artem Trokhymchuk <trkhmchk@gmail.com>
2023-06-26 22:26:48 +0300

    Add extra parens

--- include/CLI/TypeTools.hpp
+++ include/CLI/TypeTools.hpp
@@ -1649,8 +1649,8 @@ inline std::string sum_string_vector(const std::vector<std::string> &values) {
             output.append(arg);
         }
     } else {
-        if(val > static_cast<double>(std::numeric_limits<std::int64_t>::min()) &&
-           val < static_cast<double>(std::numeric_limits<std::int64_t>::max()) &&
+        if(val > static_cast<double>((std::numeric_limits<std::int64_t>::min)()) &&
+           val < static_cast<double>((std::numeric_limits<std::int64_t>::max)()) &&
            std::ceil(val) - std::floor(val) <= std::numeric_limits<double>::epsilon()) {
             output = detail::value_string(static_cast<int64_t>(val));
         } else {
