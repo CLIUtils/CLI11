796c28389ec664dcbd99b90b2b2e93d4341f0666
Philip Top <phlptp@gmail.com>
2023-08-15 11:52:13 -0700

    install package config to correct location

--- cmake/CLI11GeneratePkgConfig.cmake
+++ cmake/CLI11GeneratePkgConfig.cmake
@@ -1,3 +1,3 @@
 configure_file("cmake/CLI11.pc.in" "CLI11.pc" @ONLY)
 
-install(FILES "${PROJECT_BINARY_DIR}/CLI11.pc" DESTINATION "${CMAKE_INSTALL_DATADIR}/pkgconfig")
+install(FILES "${PROJECT_BINARY_DIR}/CLI11.pc" DESTINATION "${CMAKE_INSTALL_LIBDIR}/pkgconfig")
