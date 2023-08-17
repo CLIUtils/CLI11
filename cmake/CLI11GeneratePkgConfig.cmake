ff96dc2b24d5e9f21a5611a4bc63e57d11d5f701
Philip Top <phlptp@gmail.com>
2023-08-17 05:38:52 -0700

    fix the template file condition

--- cmake/CLI11GeneratePkgConfig.cmake
+++ cmake/CLI11GeneratePkgConfig.cmake
@@ -1,9 +1,9 @@
 if(CLI11_PRECOMPILED)
-  configure_file("cmake/CLI11.pc.in" "CLI11.pc" @ONLY)
+  configure_file("cmake/CLI11precompiled.pc.in" "CLI11.pc" @ONLY)
 elseif(CLI11_SINGLE_FILE)
   configure_file("cmake/CLI11single.pc.in" "CLI11.pc" @ONLY)
 else()
-  configure_file("cmake/CLI11precompiled.pc.in" "CLI11.pc" @ONLY)
+  configure_file("cmake/CLI11.pc.in" "CLI11.pc" @ONLY)
 endif()
 
 install(FILES "${PROJECT_BINARY_DIR}/CLI11.pc" DESTINATION "${CMAKE_INSTALL_LIBDIR}/pkgconfig")
