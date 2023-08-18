1cb75471863bb364038de3a5ef8b596694945990
Philip Top <phlptp@gmail.com>
2023-08-17 19:59:58 -0700

    simplify to single pc file for single_file and normal

--- cmake/CLI11GeneratePkgConfig.cmake
+++ cmake/CLI11GeneratePkgConfig.cmake
@@ -1,7 +1,5 @@
 if(CLI11_PRECOMPILED)
   configure_file("cmake/CLI11precompiled.pc.in" "CLI11.pc" @ONLY)
-elseif(CLI11_SINGLE_FILE)
-  configure_file("cmake/CLI11single.pc.in" "CLI11.pc" @ONLY)
 else()
   configure_file("cmake/CLI11.pc.in" "CLI11.pc" @ONLY)
 endif()
