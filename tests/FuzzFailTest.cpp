3e588dfae84a9b74d9a56e600ab3efba9d327975
Philip Top <phlptp@gmail.com>
2023-10-09 08:27:27 -0700

    fix test for to_config in some cases

--- tests/FuzzFailTest.cpp
+++ tests/FuzzFailTest.cpp
@@ -58,3 +58,36 @@ TEST_CASE("file_fail") {
     } catch(const CLI::ParseError & /*e*/) {
     }
 }
+
+
+TEST_CASE("app_file_gen_fail") {
+    CLI::FuzzApp fuzzdata;
+    auto app = fuzzdata.generateApp();
+
+    int index = GENERATE(range(1,2));
+    std::string optionString;
+    auto parseData = loadFailureFile("fuzz_app_file_fail", index);
+    if(parseData.size() > 25) {
+        optionString = parseData.substr(0, 25);
+        parseData.erase(0, 25);
+    }
+
+    try {
+
+        if(!optionString.empty()) {
+            app->add_option(optionString, fuzzdata.buffer);
+        }
+        try {
+            app->parse(parseData);
+        } catch(const CLI::ParseError & /*e*/) {
+            return;
+        }
+    } catch(const CLI::ConstructionError & /*e*/) {
+        return;
+    }
+    std::string configOut=app->config_to_str();
+    app->clear();
+    std::stringstream out(configOut);
+    app->parse_from_stream(out);
+
+}
