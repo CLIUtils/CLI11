2006ed60e1fa338591d8352b17f6e0db131420d3
Philip Top <top1@llnl.gov>
2024-02-06 15:51:49 -0800

    update tester code

--- examples/testEXE.cpp
+++ examples/testEXE.cpp
@@ -9,20 +9,26 @@
 #include <CLI/CLI.hpp>
 #include <iostream>
 #include <string>
+#include <filesystem>
 
-int main(int argc, const char *argv[]) {
+int main(int argc, char *argv[])
+{
+    CLI::App app{"MyApp"};
+    std::filesystem::path binpath1 = "../example/myfile.bin";
+        std::filesystem::path binpath2 = "../example/myfile.bin";
+        std::filesystem::path binpath3 = "../example/myfile.bin";
 
-    int value{0};
-    CLI::App app{"Test App"};
-    app.add_option("-v", value, "value");
+        app.add_option("-f,--file1", binpath1, "Path to a .bin file"); // this works but the path "../example/myfile.bin" won't show up in --help. I need it to show somewhere that this is a required argument but it's populated with a default value - running the app.exe without the arg will work and use the default value, but the user can overwrite it.
 
-    auto *subcom = app.add_subcommand("sub", "")->prefix_command();
-    CLI11_PARSE(app, argc, argv);
+    app.add_option("-g,--file2", binpath2, "Path to a .bin file")->capture_default_str(); // this doesn't seem to change anything, and I don't understand the documentation, it's not clear to me what it says.
 
-    std::cout << "value =" << value << '\n';
-    std::cout << "after Args:";
-    for(const auto &aarg : subcom->remaining()) {
-        std::cout << aarg << " ";
+    app.add_option("-i,--file3", binpath3, "Path to a .bin file")->default_str("defFile.bin")->check(CLI::ExistingFile); // this doesn't seem to change anything either, and I also don't understand the documentation, it's not clear to me what it should do or change.
+
+    try
+    {
+        app.parse(argc, argv);
+    } catch (const CLI::ParseError& e)
+    {
+        return app.exit(e);
     }
-    std::cout << '\n';
 }
