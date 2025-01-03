dd06172986374bf144dc3f5da3c6741af4d26881
Philip Top <phlptp@gmail.com>
2025-01-03 07:54:33 -0800

    update copyright dates to 2025

--- examples/json.cpp
+++ examples/json.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2021, University of Cincinnati, developed by Henry Schreiner
+// Copyright (c) 2017-2025, University of Cincinnati, developed by Henry Schreiner
 // under NSF AWARD 1414736 and by the respective contributors.
 // All rights reserved.
 //
