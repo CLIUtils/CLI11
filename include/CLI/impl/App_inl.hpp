50e90e984c7cc5d76557ecbb0cd941a09f852d85
Philip Top <phlptp@gmail.com>
2025-05-21 06:57:11 -0700

    fix seg fault possibility

--- include/CLI/impl/App_inl.hpp
+++ include/CLI/impl/App_inl.hpp
@@ -1782,7 +1782,10 @@ CLI11_INLINE bool App::_parse_positional(std::vector<std::string> &args, bool ha
         }
 
         if(posOpt->get_trigger_on_parse()) {
-            posOpt->run_callback();
+            if (!posOpt->empty())
+            {
+                posOpt->run_callback();
+            }
         }
 
         args.pop_back();
