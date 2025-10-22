9352cecc544188d6bfc2d5793f8daeb85ade92ef
Philip Top <phlptp@gmail.com>
2025-10-22 05:38:32 -0700

    try adding a test for failure with macos arm precompile

--- tests/FuzzFailTest.cpp
+++ tests/FuzzFailTest.cpp
@@ -149,7 +149,7 @@ TEST_CASE("app_file_roundtrip") {
         result = fuzzdata2.compare(fuzzdata);
     }
     */
-    INFO("Failure in test case " << index)
+    INFO("Failure in test case " << index);
     CHECK(result);
 }
 
@@ -345,14 +345,14 @@ TEST_CASE("app_roundtrip_custom") {
     CLI::FuzzApp fuzzdata2;
     auto app = fuzzdata.generateApp();
     auto app2 = fuzzdata2.generateApp();
-    int index = GENERATE(range(1, 25));
+    int index = GENERATE(range(25, 26));
 
     auto parseData = loadFailureFile("round_trip_custom", index);
 
     std::size_t pstring_start{0};
     pstring_start = fuzzdata.add_custom_options(app.get(), parseData);
     INFO("Failure in test case " << index << " file length=" << parseData.size() << " pstring start at "
-                                 << pstring_start)
+                                 << pstring_start);
     if(pstring_start > 0) {
         app->parse(parseData.substr(pstring_start));
         CHECK_NOTHROW(app->help("", CLI::AppFormatMode::All));
@@ -392,7 +392,7 @@ TEST_CASE("app_roundtrip_parse_normal_fail") {
         CHECK(true);
         return;
     }
-    INFO("Failure in test case " << index)
+    INFO("Failure in test case " << index);
     try {
         if(pstring_start > 0) {
             app->parse(parseData.substr(pstring_start));
