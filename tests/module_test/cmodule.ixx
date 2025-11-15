4f89361d818bb85e5081edfee08c0f8b5230180e
Philip Top <phlptp@gmail.com>
2025-11-15 05:28:09 -0800

    update some docs and copyright notices on code

--- tests/module_test/cmodule.ixx
+++ tests/module_test/cmodule.ixx
@@ -1,8 +1,17 @@
+// Copyright (c) 2024 scivision
+// Copyright (c) 2025 University of Cincinnati, developed by Henry Schreiner
+// under NSF AWARD 1414736 and by the respective contributors.
+// All rights reserved.
+//
+// SPDX-License-Identifier: MIT
+
+// modified from https://github.com/iTrooz/CppModules/blob/cli11 for use in CLI11 tests
+
 module;
 
 #include <CLI/CLI.hpp>
 
-export module math;
+export module cmodule;
 
 export void foo(CLI::App *app) {}
 
--- tests/module_test/module_test.cpp
+++ tests/module_test/module_test.cpp
@@ -1,8 +1,17 @@
+// Copyright (c) 2024 scivision
+// Copyright (c) 2025 University of Cincinnati, developed by Henry Schreiner
+// under NSF AWARD 1414736 and by the respective contributors.
+// All rights reserved.
+//
+// SPDX-License-Identifier: MIT
+
+// modified from https://github.com/iTrooz/CppModules/blob/cli11 for use in CLI11 tests
+
 #include <cassert>
 #include <cstdio>
 #include <cstdlib>
 
-import math;
+import cmodule;
 
 int main() {
     int a = 1;
