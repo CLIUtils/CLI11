f76bce0877ad59a239b3244b6814cd7e5a58a85b
Philip Top <phlptp@gmail.com>
2026-03-24 05:12:38 -0700

    test the module with more complex parsing tests

--- tests/module_test/module_test.cpp
+++ tests/module_test/module_test.cpp
@@ -15,10 +15,33 @@ using CLI::App;
 using CLI::ParseError;
 
 int main(int argc, char *argv[]) {
+    std::string script_path {};
+    std::string toolchain_path {};
+    std::string build_dir {};
+    std::string install_dir {};
+    std::string output{};
     App app{"Module test"};
 
     string value;
     app.add_option("value", value, "A test value")->required();
+    //test with long and short options and subcommands
+     auto config = app.add_subcommand("configure", "Configure the project");
+    config->add_option("-S,--script-path", script_path, "Path to build script");
+    config->add_option("-T,--toolchain-path", toolchain_path, "Compiler toolchain to use");
+    config->add_option("-B,--build-dir", build_dir, "Build directory");
+    // Build Subcommand
+    auto build = app.add_subcommand("build", "Execute build targets");
+    build->add_option("-B,--build-dir", build_dir, "Build directory");
+    // Install Subcommand
+    auto install = app.add_subcommand("install", "Install build artifacts");
+    install->add_option("-B,--build-dir", build_dir, "Source build directory");
+    install->add_option("-I,--install-dir", install_dir, "Installation prefix");
+    // Scan Subcommand
+    auto scan = app.add_subcommand("scan", "Scan for source changes/dependencies");
+    scan->add_option("-B,--build-dir", build_dir, "Build directory");
+    // test with option group
+    auto group = app.add_option_group("output group", "type of output");
+    group->add_option("-o,--output", value, "output type");
 
     try {
         app.parse(argc, argv);
